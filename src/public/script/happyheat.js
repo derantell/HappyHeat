e47b3fdb76be5d7582b6c3197079c469101d59d4
David Tellander <dtellander@gmail.com>
2014-02-14 14:09:50 +0100

    Set origin to 1,1 in heatmap
    Cause this is how it is in real life.

--- src/public/script/happyheat.js
+++ src/public/script/happyheat.js
@@ -1,20 +1,20 @@
 (function (pts) {
     window.onload = function() {
         var heatmapElement = document.getElementById('heatmap'),
-            xcoeff = heatmapElement.clientWidth  / 5.0,
-            ycoeff = heatmapElement.clientHeight / 5.0,
+            xcoeff = heatmapElement.clientWidth  / 4.0,
+            ycoeff = heatmapElement.clientHeight / 4.0,
             data   = [],
             config = {
                 element: heatmapElement,
-                radius:  60,
+                radius:  75,
                 opacity: 50
             },
             heatmap = h337.create(config);
 
         pts.forEach( function( point ) { 
             data.push({
-                x: xcoeff * point.p, 
-                y: heatmapElement.clientHeight - ycoeff * point.m
+                x: xcoeff * (point.p-1), 
+                y: heatmapElement.clientHeight - ycoeff * (point.m-1)
             });
         });
 
